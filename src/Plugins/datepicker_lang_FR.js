4e269cfcc93aa33b7714d37cb9822199bcd19553
nicop12 <infos@nicop.net>
2016-01-30 22:56:43 +0100

    Adding datepicker fr strings

---
+++ src/Plugins/datepicker_lang_FR.js
@@ -0,0 +1,37 @@
+﻿var i18n = $.extend({}, i18n || {}, {
+    datepicker: {
+        dateformat: {
+            "fulldayvalue": "M/d/yyyy",
+            "separator": "/",
+            "year_index": 2,
+            "month_index": 0,
+            "day_index": 1,
+            "sun": "Dim",
+            "mon": "Lun",
+            "tue": "Mar",
+            "wed": "Mer",
+            "thu": "Jeu",
+            "fri": "Ven",
+            "sat": "Sam",
+            "jan": "Jan",
+            "feb": "Fev",
+            "mar": "Mar",
+            "apr": "Avr",
+            "may": "Mai",
+            "jun": "Jui",
+            "jul": "Jul",
+            "aug": "Auo",
+            "sep": "Sep",
+            "oct": "Oct",
+            "nov": "Nov",
+            "dec": "Dec",
+            "postfix": ""
+        },
+        ok: " Ok ",
+        cancel: "Annul",
+        today: "Aujourd'hui",
+        prev_month_title: "mois préc.",
+        next_month_title: "mois suiv."
+    }
+});
+
